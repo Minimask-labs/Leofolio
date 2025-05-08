32e645468bf6bd59e41cdd29627aa458e7d10139
kufre-abasi <kufreabasibassey3@gmail.com>
2025-05-08 21:29:39 +0100

    docs:

--- app/project-dashboard/[id]/page.tsx
+++ app/project-dashboard/[id]/page.tsx
@@ -15,7 +15,7 @@ export default function projectDashboard() {
     <main className="min-h-screen   ">
       <Header />
       <div className="container mx-auto px-4 py-8">
-        <Dashboard userType={'employer'} />
+        <Dashboard   />
       </div>
     </main>
   );
